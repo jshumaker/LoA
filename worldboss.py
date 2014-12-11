81cc8bcb85ffc3fe1f963cb82ba95d13f3d0fbc4
jshumaker <jody.shumaker@gmail.com>
2014-12-10 19:32:45 -0500

    Overhaul of worldboss to use new loa module. Now uses morale boost detection of when to start.

--- worldboss.py
+++ worldboss.py
@@ -4,8 +4,8 @@ from utility.mouse import *
 from utility.screen import *
 from utility.logconfig import *
 import argparse
-from PIL import ImageGrab, Image
 import os.path
+from loa import *
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -20,7 +20,6 @@ if args.debug:
     loglevel = logging.DEBUG
 logconfig('worldboss', loglevel)
 
-
 # Resources.
 icon_corner_image = Image.open(os.path.join(script_dir, "misc/IconCorner.png"))
 daily_tasks_image = Image.open(os.path.join(script_dir, "misc/DailyTasks.png"))
@@ -28,40 +27,33 @@ join_image = Image.open(os.path.join(script_dir, "misc/Join.png"))
 morale_image = Image.open(os.path.join(script_dir, "misc/Morale.png"))
 morale_inactive_image = Image.open(os.path.join(script_dir, "misc/MoraleInactive.png"))
 
-game_window = get_game_window(auto=True)
 
-game_center = (int((game_window[2] - game_window[0]) / 2) + game_window[0],
-               int((game_window[3] - game_window[1]) / 2) + game_window[1])
-
-# Give the game focus.
-safe_click_pos = (max(0, game_window[0] - 1), max(0, game_window[1]))
-Mouse.click(*safe_click_pos)
-time.sleep(0.050)
+game = LeagueOfAngels()
 
 # Let's orient the first icon.
 logging.log(VERBOSE, "Searching for first icon.")
-screengrab = ImageGrab.grab()
-searchpos = (game_window[2] - 242, game_window[1] + 8)
-first_icon_pos = image_search(screengrab, icon_corner_image, *searchpos, radius=20, great_threshold=1000.0)
+screenshot = game.capture_screenshot()
+first_icon_pos = game.image_find(icon_corner_image, 237, 8, xorient=Orient.Right, screenshot=screenshot,
+                                 radius=20, great_threshold=1000.0)
 
-if first_icon_pos[0] == -1:
+if first_icon_pos.x == -1:
     logging.error("Failed to find first icon.")
     sys.exit(1)
-logging.log(VERBOSE, "First icon found, offset from expected: {0},{1}".format(first_icon_pos[0] - searchpos[0],
-                                                                              first_icon_pos[1] - searchpos[1]))
+logging.log(VERBOSE, "First icon found, offset from expected: {0},{1}".format(first_icon_pos.xoffset,
+                                                                              first_icon_pos.yoffset))
 # Find the daily tasks button.
 logging.log(VERBOSE, "Searching for daily tasks button.")
 found = False
-x, y = first_icon_pos
+x, y = first_icon_pos.x, first_icon_pos.y
 icon_count = 0
 while not found and icon_count < 40:
-    if image_search(screengrab, daily_tasks_image, x, y, radius=0, threshold=120000)[0] != -1:
+    if game.image_find(daily_tasks_image, x, y, radius=0, threshold=120000, screenshot=screenshot)[0] != -1:
         logging.log(VERBOSE, "Daily tasks button found, {0},{1}".format(x, y))
         found = True
     else:
         x -= 62
-        if x - game_window[0] < 310:
-            x = first_icon_pos[0]
+        if x < 310:
+            x = first_icon_pos.x
             y += 70
     icon_count += 1
 
@@ -70,18 +62,17 @@ if not found:
     sys.exit(1)
 
 # Enter Daily Tasks
-Mouse.click(x + 25, y + 25)
+game.click(x + 25, y + 25)
 time.sleep(0.010)
 
 # Wait for Join button.
 logging.log(VERBOSE, "Searching for join button.")
-timeout = time.time() + 30.0
+timeout = time.time() + 10.0
 while time.time() < timeout:
-    searchx = game_center[0] + 161
-    searchy = game_center[1] - 152
-    posx, posy = image_search(ImageGrab.grab(), join_image, searchx, searchy, radius=5, great_threshold=2000)
-    if posx != -1:
-        logging.log(VERBOSE, "Join button found, offset {0},{1}".format(posx - searchx, posy - searchy))
+    join_pos = game.image_find(join_image, 161, -150, xorient=Orient.Center, yorient=Orient.Center,
+                               radius=5, threshold=15000, great_threshold=2000)
+    if join_pos.x != -1:
+        logging.log(VERBOSE, "Join button found, offset {0},{1}".format(join_pos.xoffset, join_pos.yoffset))
         break
     time.sleep(0.200)
 if time.time() > timeout:
@@ -89,41 +80,43 @@ if time.time() > timeout:
     sys.exit(1)
 
 # Join
-Mouse.click(game_center[0] + 176, game_center[1] - 143)
+logging.info("Joining World Boss.")
+game.click(join_pos.x, join_pos.y)
 time.sleep(2.000)
-
-# Run up to line.
-logging.info("Running up to line.")
-Mouse.click(game_window[2] - 280, game_window[1] + 170)
-time.sleep(0.100)
-# Auto attack
-logging.info("Enabling auto attack.")
-Mouse.click(game_center[0] + 117, game_window[3] - 97)
 
 
 logging.info("Searching for Morale button...")
 timeout = time.time() + 15.0
 while time.time() < timeout:
-    morale_x, morale_y = image_search(ImageGrab.grab(), morale_image, game_center[0] - 100, game_window[3] - 120,
-                                      radius=25)
-    if morale_x != -1:
-        logging.info("Found Morale button at {0},{1}")
+    morale_pos = game.image_find(morale_image, -95, 121, xorient=Orient.Center, yorient=Orient.Bottom,
+                                 radius=5)
+    if morale_pos.x != -1:
+        logging.info("Found Morale button at {},{}".format(morale_pos.xoffset, morale_pos.yoffset))
         break
+
+# Run up to line.
+logging.info("Running up to line.")
+game.click(280, 170, xorient=Orient.Right)
+time.sleep(1.000)
+# Auto attack
+logging.info("Enabling auto attack.")
+game.click(117, 97, xorient=Orient.Center, yorient=Orient.Bottom)
 
 # Morale boost
 for i in range(6):
     timeout = time.time() + 20.0
     while time.time() < timeout:
-        x, y = image_search(ImageGrab.grab(), morale_image, morale_x, morale_y, radius=0)
-        if x != -1:
+        pos = game.image_find(morale_image, morale_pos.x, morale_pos.y, radius=0)
+        if pos.x != -1:
             break
         time.sleep(0.500)
-    if x == -1:
+    if pos.x == -1:
         logging.info("Morale button did not appear as expected, assuming world boss is over.")
         break
     # Keep clicking till it seems to take.
     timeout = time.time() + 10
-    while time.time() < timeout and Mouse.cursor_is_hand((morale_x, morale_y)):
-        Mouse.click(morale_x, morale_y)
+    while time.time() < timeout and game.image_find(morale_inactive_image,
+                                                    morale_pos.x, morale_pos.y, radius=0).x == -1:
+        game.click(morale_pos.x, morale_pos.y)
         time.sleep(1.0)
     time.sleep(179.0)
